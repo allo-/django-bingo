ce3553346415de32de3c00ae3bf3552ab1aff0d3
Alexander Schier <allo@laxu.de>
2013-08-01 00:27:03 +0200

    added models for Game, BingoBoard, BingoField and changed the Word model

--- bingo/admin.py
+++ bingo/admin.py
@@ -2,4 +2,36 @@ from django.contrib import admin
 
 from models import *
 
-admin.site.register(Word)
+
+class WordAdmin(admin.ModelAdmin):
+    list_filter = ("is_middle", "is_active")
+    list_display = ("word", "is_active", "is_middle")
+
+    class Meta:
+        ordering = ("word",)
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+
+def bingo_user(bingo_board):
+    return bingo_board.user if bingo_board.user else bingo_board.ip
+
+
+def bingo_id(bingo_board):
+    return u'BingoBoard #{0}'.format(bingo_board.id)
+
+
+class BingoBoardAdmin(admin.ModelAdmin):
+    list_display = (bingo_id, "color", "game", bingo_user)
+    list_editable = ("color",)
+
+
+class BingoFieldAdmin(admin.ModelAdmin):
+    list_display = ("word", "board", "position")
+    list_filter = ("word", "position")
+
+admin.site.register(Word, WordAdmin)
+admin.site.register(Game)
+admin.site.register(BingoBoard, BingoBoardAdmin)
+admin.site.register(BingoField, BingoFieldAdmin)
--- bingo/models.py
+++ bingo/models.py
@@ -1,9 +1,74 @@
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext as _
+from django.core.exceptions import ValidationError
+
+from colorful.fields import RGBColorField
+
 from django.db import models
 
 
 class Word(models.Model):
+    """
+        a word entry. Should not be deleted, but only disabled
+        to preserve old BingoFields
+    """
     word = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
+    is_middle = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.word
+
+
+class Game(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return _(u"Game created at {0}").format(
+            self.date.strftime(u"%Y-%m-%d %H:%M"))
+
+
+class BingoBoard(models.Model):
+    game = models.ForeignKey("Game")
+    color = RGBColorField()
+    ip = models.IPAddressField(blank=True, null=True)
+    user = models.ForeignKey(get_user_model(), blank=True, null=True)
+
+    def clean(self):
+        if not self.ip and not self.user:
+            raise ValidationError(_(u"Game must have either an ip or an user"))
+
+    def __unicode__(self):
+        return u"BingoBoard #{0} created by {1}".format(
+            self.id,
+            self.user if self.user else self.ip)
+
+
+def position_validator(value):
+    if not (0 < value < 26):
+        raise ValidationError(_(
+            u"invalid position. valid values range: 1-25"))
+
+
+class BingoField(models.Model):
+    word = models.ForeignKey("Word")
+    board = models.ForeignKey("BingoBoard")
+    position = models.SmallIntegerField(validators=[position_validator])
+
+    def is_middle(self):
+        return self.position == 13
+
+    def clean(self):
+        if self.is_middle() and not self.word.is_middle:
+            raise ValidationError(_(
+                u"The BingoField has middle position, "
+                u"but the word is no middle word"))
+        elif not self.is_middle() and self.word.is_middle:
+            raise ValidationError(_(
+                u"The BingoField is not in the middle,"
+                u"but the word is a middle word"))
+
+    def __unicode__(self):
+        return _(u"BingoField: word={0}, pos=({1},{2}){3}").format(
+            self.word, self.position/5+1, self.position % 5,
+            _(u" [middle]") if self.is_middle() else u"")
