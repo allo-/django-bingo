b810c85d352bdc5de9a23e8daa7139afb806e193
Alexander Schier <allo@laxu.de>
2013-08-06 01:21:13 +0200

    use a salted hash for the passwords

--- bingo/forms.py
+++ bingo/forms.py
@@ -1,13 +1,23 @@
 from django import forms
 from django.utils.translation import ugettext as _
+from django.conf import settings
+from django.contrib.auth.hashers import get_hasher
 
 from models import BingoBoard
+
+
+SALT = getattr(settings, "SALT", "hackme")
 
 
 class CreateForm(forms.Form):
     password = forms.CharField(label=_(u"Password (optional)"),
                                widget=forms.PasswordInput(),
                                required=False)
+    def clean_password(self):
+        hasher = get_hasher()
+        hashed_password = hasher.encode(self.cleaned_data['password'],
+            SALT)
+        return hashed_password
 
 
 class ReclaimForm(forms.Form):
@@ -18,12 +28,14 @@ class ReclaimForm(forms.Form):
         super(ReclaimForm, self).__init__(data)
 
     def clean_password(self):
-        password = self.cleaned_data['password']
+        hasher = get_hasher()
+        hashed_password = hasher.encode(self.cleaned_data['password'],
+            SALT)
         if not self.game or self.game.is_expired():
             raise forms.ValidationError(
                 _("The game is expired, please create a new field"))
         bingo_boards = BingoBoard.objects.filter(game=self.game,
-                                                 password=password)
+                                                 password=hashed_password)
         if bingo_boards.count() > 0:
             # if two users have the same password,
             # just return the first board.
@@ -32,3 +44,4 @@ class ReclaimForm(forms.Form):
             raise forms.ValidationError(
                 _(u"No active board with this password."
                     " Try again, or create a new one."))
+        return hashed_password
