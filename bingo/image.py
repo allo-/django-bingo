63a19e8f37abab7362fb50f1fdf98bfd526b93df
Alexander Schier <allo@laxu.de>
2013-08-10 23:01:15 +0200

    multiline strings in image, date on image, constants in uppercase
    * support for multiline strings with PIL, by drawing them line for line.
    * put the creation date on the middle field.
    * moved constants (settings) to the top and use UPPERCASE for the names.

--- bingo/image.py
+++ bingo/image.py
@@ -7,27 +7,54 @@ from PIL import Image, ImageDraw, ImageFont, ImageDraw, ImageFont
 from models import Word, BingoField
 
 
+# constants
 COLOR_MODE_BLANK, COLOR_MODE_MARKED, COLOR_MODE_VOTED = range(3)
 
+# settings
+H_BOX_PADDING = getattr(settings, "HORIZONTAL_PADDING", 8)
+V_BOX_PADDING = getattr(settings, "VERTICAL_PADDING", 30)
+H_LINE_MARGIN = getattr(settings, "HORIZONTAL_LINE_MARGIN", 0)
+V_LINE_MARGIN = getattr(settings, "VERTICAL_LINE_MARGIN", 5)
+BORDER = getattr(settings, "BORDER", 1)
+FONTPATH = getattr(settings, "FONT_PATH", "/path/to/font.ttf")
+FONTSIZE = getattr(settings, "FONT_SIZE", 16)
+BINGO_DATETIME_FORMAT = getattr(
+    settings, "BINGO_DATETIME_FORMAT", "%Y-%m-%d %H:%M")
 
-def get_texts_and_sizes(bingo_fields, font):
+
+class Text(object):
+    def __init__(self, draw, font, input):
+        self.line_widths = []
+        self.line_heights = []
+        self.lines = input.split("\n")
+        for line in self.lines:
+            line_width, line_height = draw.textsize(line, font=font)
+            self.line_widths.append(line_width + H_LINE_MARGIN)
+            self.line_heights.append(line_height + V_LINE_MARGIN)
+
+    def get_total_width(self):
+        return sum(self.line_widths)
+
+    def get_total_height(self):
+        return sum(self.line_heights)
+
+
+def get_texts(bingo_fields, font):
     im = Image.new("RGB", (200, 200), color=(255, 255, 255))
     draw = ImageDraw.Draw(im)
     words = []
     word_widths = []
     word_heights = []
     texts = []
+
     for bingo_field in bingo_fields:
         text = bingo_field.word.word
         if bingo_field.is_middle():
-            text = _("Board #{0}: ").format(bingo_field.board.id) + text
-
-        word_width, word_height = draw.textsize(text, font=font)
-        word_widths.append(word_width)
-        word_heights.append(word_height)
-        texts.append(text)
-
-    return texts, word_widths, word_heights
+            text += _("\n{time}\nBoard #{board_id}").format(
+                time=bingo_field.board.created.strftime(BINGO_DATETIME_FORMAT),
+                board_id=bingo_field.board.id)
+        texts.append(Text(draw, font, text))
+    return texts
 
 
 def get_colors(bingo_field, vote_counts, colormode=COLOR_MODE_BLANK):
@@ -45,7 +72,7 @@ def get_colors(bingo_field, vote_counts, colormode=COLOR_MODE_BLANK):
     # middle field
     middle_field_color = (90, 90, 90)
     middle_board_text_color = (255, 255, 255)
-    # border
+    # border 
     border_color = (0, 0, 0)
 
     field_color = neutral_field_color
@@ -69,34 +96,31 @@ def get_colors(bingo_field, vote_counts, colormode=COLOR_MODE_BLANK):
 
 
 def get_image(bingo_board, marked=False, voted=False):
-    h_padding = getattr(settings, "HORIZONTAL_PADDING", 8)
-    v_padding = getattr(settings, "VERTICAL_PADDING", 30)
-    border = getattr(settings, "BORDER", 1)
-
-    fontpath = getattr(settings, "FONT_PATH", "/path/to/font.ttf")
-    fontsize = getattr(settings, "FONT_SIZE", 16)
-    font = ImageFont.truetype(fontpath, fontsize)
+    font = ImageFont.truetype(FONTPATH, FONTSIZE)
 
     all_bingo_fields = BingoField.objects.filter(board__game=bingo_board.game)
     bingo_fields = all_bingo_fields.filter(board=bingo_board).exclude(
         position=None).order_by("position")
 
     # image sizes
-    texts, word_widths, word_heights = get_texts_and_sizes(bingo_fields, font)
-    max_word_width = max(word_widths)
-    max_word_height = max(word_heights)
+    texts = get_texts(bingo_fields, font)
+    max_text_width = 0
+    max_text_height = 0
+    for text in texts:
+        max_text_width = max(max_text_width, text.get_total_width())
+        max_text_height = max(max_text_height, text.get_total_height())
 
     # inside the border
-    inner_width = max_word_width + 2 * h_padding
-    inner_height = max_word_height + 2 * v_padding
+    inner_width = max_text_width + 2 * H_BOX_PADDING
+    inner_height = max_text_height + 2 * V_BOX_PADDING
 
     # with border
-    field_width = inner_width + 2 * border
-    field_height = inner_height + 2 * border
+    field_width = inner_width + 2 * BORDER
+    field_height = inner_height + 2 * BORDER
 
     # total board width
-    image_width = 5*(max_word_width + (2 * h_padding)) + (6 * border)
-    image_height = 5*(max_word_height + (2 * v_padding)) + (6 * border)
+    image_width = 5*(max_text_width + (2 * H_BOX_PADDING)) + (6 * BORDER)
+    image_height = 5*(max_text_height + (2 * V_BOX_PADDING)) + (6 * BORDER)
 
     im = Image.new("RGB", (image_width, image_height), color=(255, 255, 255))
     draw = ImageDraw.Draw(im)
@@ -116,12 +140,12 @@ def get_image(bingo_board, marked=False, voted=False):
         pos = bingo_field.position - 1
         y, x = divmod(pos, 5)
         # top left corner of the box
-        box_left = x * (field_width - border)
-        box_top = y * (field_height - border)
+        box_left = x * (field_width - BORDER)
+        box_top = y * (field_height - BORDER)
         # top left corner inside the box
         # inside the border, but outside of the padding
-        word_left = box_left + border
-        word_top = box_top + border
+        word_left = box_left + BORDER
+        word_top = box_top + BORDER
 
         # calculate the background and foreground color
         # for the field
@@ -136,7 +160,7 @@ def get_image(bingo_board, marked=False, voted=False):
 
         # draw a border. if its more than 1px, its extended
         # by drawing smaller boxes inside
-        for offset in xrange(border):
+        for offset in xrange(BORDER):
             draw.rectangle((
                 (box_left + offset, box_top + offset),
                 (box_left + field_width - offset,
@@ -145,13 +169,16 @@ def get_image(bingo_board, marked=False, voted=False):
                 outline=border_color
             )
 
-        h_center = (inner_width - word_widths[pos]) / 2.0
-        v_center = (inner_height - word_heights[pos]) / 2.0
+        v_center = (inner_height - texts[pos].get_total_height()) / 2.0
         text = texts[pos]
-        draw.text(
-            (word_left + h_center,
-             word_top + v_center),
-            text, font=font, fill=word_color
-        )
+        h_offset = 0
+        for idx, line in enumerate(text.lines):
+            h_center = (inner_width - text.line_widths[idx]) / 2.0
+            draw.text(
+                (word_left + h_center,
+                 word_top + v_center + h_offset),
+                line, font=font, fill=word_color
+            )
+            h_offset += text.line_heights[idx]
 
     return im
