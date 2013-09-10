0f2d15dc5a1dca4ed6026beeee72549996853750
Alexander Schier <allo@laxu.de>
2013-09-10 22:30:14 +0200

    version 1.0.0

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
-import os
+import os, sys
 from setuptools import setup
+from setuptools.command.install_lib import install_lib as _install_lib
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
+
+class install_lib(_install_lib):
+    def run(self):
+        from django.core.management.commands.compilemessages import compile_messages
+        os.chdir('bingo')
+        compile_messages(sys.stderr)
+        os.chdir("..")
 
 setup(name='django-bingo',
       description='Bingo',
       long_description='Bingo game implemented in django',
       author='Alexander Schier',
       author_email='allo@laxu.de',
-      version='0.0.1',
+      version='1.0.0',
       packages=['bingo'],
+      package_data={'bingo': ['templates/bingo/*.*',
+                              'static/bingo/*.*',
+                              'static/bingo/js/*.*',
+                              'locale/*/LC_MESSAGES/*.*']},
       include_package_data=True,
       install_requires=required,
       classifiers=[
