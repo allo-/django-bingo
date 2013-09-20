5610cdeae5f26871936247039ec8aaa383f321cb
Alexander Schier <allo@laxu.de>
2013-09-20 20:33:13 +0200

    some admin interface improvements

--- bingo/admin.py
+++ bingo/admin.py
@@ -5,9 +5,45 @@ from django import forms
 from models import *
 
 
+def bingoboard_user(bingo_board):
+    return bingo_board.user if bingo_board.user else bingo_board.ip
+
+
+def bingoboard_name(bingo_board):
+    return u'BingoBoard #{0} (site {1})'.format(
+        bingo_board.board_id, bingo_board.game.site)
+
+
+def word_sites(word):
+    """
+        @returns: the sites, where a Word instance is active
+    """
+    return ", ".join(map(lambda site: site.domain, word.site.all()))
+
+
+def game_id(game):
+    """
+        just the game.id prefixed by "Game #"
+        so its easier to click in the admin interface
+    """
+    return u"Game #{0}".format(game.game_id)
+
+
+def bingoboard_game_id(bingo_board):
+    """
+        @returns the game.id for the game of a BingoBoard instance
+    """
+    return game_id(bingo_board.game)
+
+
+bingoboard_name.short_description = "Bingo"
+bingoboard_game_id.short_description = "Game"
+word_sites.short_description = "Sites"
+
+
 class WordAdmin(admin.ModelAdmin):
-    list_filter = ("site", "is_middle")
-    list_display = ("word", "is_middle")
+    list_filter = ("site", "site", "is_middle")
+    list_display = ("word", word_sites, "is_middle")
     search_fields = ("word", )
 
     class Meta:
@@ -15,18 +51,6 @@ class WordAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
-
-
-def bingo_board_user(bingo_board):
-    return bingo_board.user if bingo_board.user else bingo_board.ip
-
-
-def bingo_board_name(bingo_board):
-    return u'BingoBoard #{0} (site {1})'.format(
-        bingo_board.board_id, bingo_board.game.site)
-
-
-bingo_board_name.short_description = "Bingo"
 
 
 class BingoFieldInline(admin.TabularInline):
@@ -42,8 +66,8 @@ class BingoFieldInline(admin.TabularInline):
 
 
 class BingoBoardAdmin(admin.ModelAdmin):
-    list_display = (bingo_board_name, "color", "created",
-                    "game", bingo_board_user)
+    list_display = (bingoboard_name, "color", "created",
+                    bingoboard_game_id, bingoboard_user)
     list_editable = ("color",)
     list_filter = ("game__site",)
     # cannot be changed, because its hashed.
@@ -54,16 +78,11 @@ class BingoBoardAdmin(admin.ModelAdmin):
 
 class BingoFieldAdmin(admin.ModelAdmin):
     list_display = ("word", "board", "position")
-    list_filter = ("word", "position")
     readonly_fields = ("board", "word", "position", "vote")
 
 
-def game_name(game):
-    return u"Game #{0} (site {1})".format(game.game_id, game.site)
-
-
 class GameAdmin(admin.ModelAdmin):
-    list_display = (game_name, "site", "created", "last_used")
+    list_display = (game_id, "site", "created", "last_used")
     list_filter = ("site",)
     readonly_fields = ("created", "last_used")
 
