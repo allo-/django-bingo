6b0dc45f3de235c40bba8b6d3075561a65de530a
Alexander Schier <allo@laxu.de>
2013-09-22 10:14:22 +0200

    use context_processors instead of template filters for settings.

--- bingo/context_processors.py
+++ bingo/context_processors.py
@@ -1,5 +1,14 @@
 from django.conf import settings
 
+def settings_context(request):
+    """
+        returns a dict with whitelisted settings
+    """
+    return {
+        'THEME': getattr(settings, "THEME", "")
+    }
+
 def bingo(request):
     return {
+        'settings': settings_context(request),
     }
