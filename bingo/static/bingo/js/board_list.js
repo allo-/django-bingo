8c7da3f9c480ec8171fba098d7a1c5ed971422ad
Alexander Schier <allo@laxu.de>
2014-05-16 01:30:13 +0200

    removed console.log debugging for mouseover

--- bingo/static/bingo/js/board_list.js
+++ bingo/static/bingo/js/board_list.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     var imgs = $('img.thumbnail');
-    imgs.bind('mouseover', function(){console.log(this.src);this.src = this.src.replace("voted", "marked")});
+    imgs.bind('mouseover', function(){this.src = this.src.replace("voted", "marked")});
     imgs.bind('mouseout', function(){this.src = this.src.replace("marked", "voted")});
 });
 $(document).ready(function(){
     var imgs = $('img.thumbnail');
-    imgs.bind('mouseover', function(){console.log(this.src);this.src = this.src.replace("voted", "marked")});
+    imgs.bind('mouseover', function(){this.src = this.src.replace("voted", "marked")});
     imgs.bind('mouseout', function(){this.src = this.src.replace("marked", "voted")});
 });
