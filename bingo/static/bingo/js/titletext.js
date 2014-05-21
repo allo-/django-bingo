89b63d4bc3956d355554f3dc664bdeca0433f305
Alexander Schier <allo@laxu.de>
2014-05-22 01:40:00 +0200

    delay title text, so it does not flicker
    added a short delay for displaying the title text or fields, so it does
    not flicker when moving the cursor quickly over the fields.

--- bingo/static/bingo/js/titletext.js
+++ bingo/static/bingo/js/titletext.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
+    var title_timeout = null;
     $("li.word, td.bingofield").bind("mouseover", function(){
-        $("#titletext").text($(this).attr("title"));
-        $(this).attr("title", "");
+        var field = $(this);
+        title_timeout = setTimeout(function(){
+            $("#titletext").text(field.attr("title"));
+            field.attr("title", "");
+            title_timeout = null;
+        }, 500);
     }).bind("mouseout", function(){
-        $(this).attr("title", $("#titletext").text());
-        $("#titletext").html("&nbsp;");
+        if(title_timeout){
+            clearTimeout(title_timeout);
+        }else{
+            $(this).attr("title", $("#titletext").text());
+            $("#titletext").html("&nbsp;");
+        }
     })
 })
