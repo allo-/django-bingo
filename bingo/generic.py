8c3f0b8f406c9318f87b0f0f3260097836eaae8b
Alexander Schier <allo@laxu.de>
2020-02-27 23:08:50 +0100

    Adapted GameList to work with get_current_site

--- bingo/generic.py
+++ bingo/generic.py
@@ -1,11 +1,13 @@
 from django.views.generic import ListView
 from django.contrib.sites.models import Site
+from django.contrib.sites.shortcuts import get_current_site
 
 from .models import Game
 
 
 class GameList(ListView):
-    queryset = Game.objects.filter(
-        site=Site.objects.get_current()).exclude(
-        bingoboard=None).order_by("-created")
     paginate_by = 25
+    def get_queryset(self):
+        return Game.objects.filter(
+            site=get_current_site(self.request)).exclude(
+            bingoboard=None).order_by("-created")
